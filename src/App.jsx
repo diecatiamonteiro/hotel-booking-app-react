3f999b4c5a1f519fa9e6a84d6eca7356e1a4efd2
Catia Monteiro <monteiro.cmonteiro@icloud.com>
2024-12-03 14:00:00 +0100

    added footer and 2 pages content: destinations and offers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Destinations from "./pages/Destinations";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/offers" element={<Offers />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
