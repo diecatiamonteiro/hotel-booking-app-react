ac955c23610df87af8b748876773ebefc6c33a03
Catia Monteiro <monteiro.cmonteiro@icloud.com>
2024-12-03 11:45:32 +0100

    added hero section to home page (including search bar now) and updated navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import SearchBar from "../components/SearchBar";
 import "../styles/Home.css";
+import HeroHomePage from "../components/HeroHomePage";
 
 const Home = () => {
   return (
-    <>
-      <SearchBar />
-    </>
+    <main>
+      <HeroHomePage />
+      <div className="home-content">
+        <h2>Discover Your Dream Vacation</h2>
+        <p>Explore our wide range of accommodations and find the perfect place to stay for your next trip.</p>
+      </div>
+    </main>
   );
 };
 
